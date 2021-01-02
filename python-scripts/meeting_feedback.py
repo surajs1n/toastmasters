cb613c07b2b8066626538c2629b9fa3020c74b23
surajs1n <suraj.s@phonepe.com>
2021-01-02 12:30:40 +0530

    rating added into meeting_feedback script

--- python-scripts/meeting_feedback.py
+++ python-scripts/meeting_feedback.py
@@ -9,6 +9,7 @@
 #    E.g: python3 meeting_feedback.py /Users/suraj.s/Downloads/HSR\ Toastmasters.csv
 # 3. You will get the output right on the terminal/command-prompt.
 
+
 import sys
 import csv
 import os
@@ -20,19 +21,29 @@ def create_path(file_path):
         os.makedirs(os.path.dirname(file_path))
 
 
-def dump_remarks_on_file(positive_remarks, negative_remarks):
-    print(f'What went well?\n=================')
-    for remark in positive_remarks:
-        print(remark)
-
-    print(f'\n\nWhat could have been improved?\n=================')
-    for remark in negative_remarks:
-        print(remark)
+# Get average of a list
+def average(list):
+    return sum(list) / len(list)
 
 
+# Function to print ratings and remarks of the dictionary.
+def print_remarks(ratings, positive_remarks_dict, negative_remarks_dict):
+    print(f'*Average Meeting Rating*: {average(ratings):.2f}\n')
+
+    print(f'*What went well?*\n-----------------')
+    for remark, counts in positive_remarks_dict.items():
+        print(f'- {remark} x{counts}')
+
+    print(f'\n\n*What could have been improved?*\n-----------------')
+    for remark, counts in negative_remarks_dict.items():
+        print(f'- {remark} x{counts}')
+
+
+# Columns with ratings          -> 8,  12, 15
 # Columns with positive remarks -> 9,  13, 16
 # Columns with negative remarks -> 10, 14, 17
 def read_from_csv_file(input_file_path):
+    ratings = []
     positive_remarks = []
     negative_remarks = []
 
@@ -41,20 +52,24 @@ def read_from_csv_file(input_file_path):
         next(csv_reader)
 
         for line in csv_reader:
+            ratings.extend([line[8], line[12], line[15]])
             positive_remarks.extend([line[9], line[13], line[16]])
             negative_remarks.extend([line[10],line[14], line[17]])
 
-    # Filter-out empty elements.
+    # Filter-out elements
+    ratings          = list(map(int, [rating for rating in ratings if rating]))
     positive_remarks = [remark for remark in positive_remarks if len(remark)>3]
     negative_remarks = [remark for remark in negative_remarks if len(remark)>3]
 
-    # Sort the list lexicographically.
-    positive_remarks.sort()
-    negative_remarks.sort()
+    positive_remarks_dict = dict((remark, positive_remarks.count(remark)) for remark in set(positive_remarks))
+    negative_remarks_dict = dict((remark, negative_remarks.count(remark)) for remark in set(negative_remarks))
 
+    # print(f'{positive_remarks_dict}')
+    # print(f'{negative_remarks_dict}')
+    # print(f'{ratings}')
     # print(f'{positive_remarks}')
     # print(f'{negative_remarks}')
-    dump_remarks_on_file(positive_remarks, negative_remarks)
+    print_remarks(ratings, positive_remarks_dict, negative_remarks_dict)
 
 
 if __name__=="__main__":
